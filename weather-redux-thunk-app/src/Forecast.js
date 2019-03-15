3b9f64b9acbce898daad8b8adf601f3134f902dd
Marek Dano <mk.dano@gmail.com>
2019-03-15 23:19:29 +0000

    Convert Forecast component to the class component and display weather details there.

--- weather-redux-thunk-app/src/Forecast.js
+++ weather-redux-thunk-app/src/Forecast.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const Forecast = (props) => {
-	return (
-		<div>
-			TODO: display forecast
-		</div>
-	)
+class Forecast extends React.Component {
+	render() {
+		if (this.props.isLoading) {
+			return <div>Loading...</div>
+		}
+		if(this.props.error) {
+			return <div>{this.props.error.message}</div>
+		}
+		return (
+			<div>
+				{this.props.cityForecast && this.props.cityForecast[0] &&
+					<div>
+						<h5>Today</h5>
+						<p>{this.props.cityForecast[0].weather_state_name}</p>
+						<p>{Number.parseInt(this.props.cityForecast[0].the_temp)} &deg;C</p>
+					</div>
+				}
+			</div>
+		)
+	}
 }
 
 const mapStateToProps = state => {
