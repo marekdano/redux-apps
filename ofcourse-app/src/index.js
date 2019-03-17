ee13d4894fbbec3a92708793d7ffe74adc368364
Marek Dano <mk.dano@gmail.com>
2019-03-17 18:31:18 +0000

    Add redux and set up the store and reducer in the ofcourse-app.

--- ofcourse-app/src/index.js
+++ ofcourse-app/src/index.js
@@ -1,4 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducer';
 
-ReactDOM.render(<div />, document.getElementById('root'));
+const store = createStore(
+	reducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ &&
+		window.__REDUX_DEVTOOLS_EXTENSION__({ trace: true })	
+);
+
+const App = () => "hello";
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>, 
+	document.getElementById('root')
+);
