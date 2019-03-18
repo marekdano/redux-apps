352599a701e71753ae0021b1a45d37432cbfb577
Marek Dano <mk.dano@gmail.com>
2019-03-18 17:34:05 +0000

    Add addCourse action. Add ADD_COURSE to reducer. Add 'App' component.

--- ofcourse-app/src/index.js
+++ ofcourse-app/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducer';
+import App from './App';
+import './index.css';
 
 const store = createStore(
 	reducer,
 	window.__REDUX_DEVTOOLS_EXTENSION__ &&
 		window.__REDUX_DEVTOOLS_EXTENSION__({ trace: true })	
 );
-
-const App = () => "hello";
 
 ReactDOM.render(
 	<Provider store={store}>
--- ofcourse-app/src/reducer.js
+++ ofcourse-app/src/reducer.js
@@ -1,5 +1,17 @@
-const initState = {};
+import { ADD_COURSE } from './actions';
+
+const initState = {
+	courses: [],
+};
 
 export default function reducer (state = initState, action) {
-	return state;
+	switch(action.type) {
+		case ADD_COURSE: 
+			return {
+				...state,
+				courses: [ ...state.courses, action.payload ],
+			}
+		default:
+			return state;
+	}
 } 
