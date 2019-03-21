f430c756063a728d7787c98cc6ce4ac13cb6d51d
Marek Dano <mk.dano@gmail.com>
2019-03-21 19:22:45 +0000

    Handle actions of begin and error in the reducer in the ofcourse-app.

--- ofcourse-app/src/reducer.js
+++ ofcourse-app/src/reducer.js
@@ -1,15 +1,30 @@
-import { ADD_COURSE_SUCCESS } from './actions';
+import { ADD_COURSE_SUCCESS, ADD_COURSE_BEGIN, ADD_COURSE_ERROR } from './actions';
 
 const initState = {
+	loading: false,
+	error: null,
 	courses: [],
 };
 
 export default function reducer (state = initState, action) {
 	switch(action.type) {
+		case ADD_COURSE_BEGIN:
+			return {
+				...state,
+				loading: true,
+      	error: null,
+			}
 		case ADD_COURSE_SUCCESS: 
 			return {
 				...state,
+				loading: false,
 				courses: [ ...state.courses, action.payload ],
+			}
+		case ADD_COURSE_ERROR:
+			return {
+				...state,
+				loading: false,
+      	error: action.error,
 			}
 		default:
 			return state;
