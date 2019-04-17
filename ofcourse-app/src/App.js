dfbec5167d3c68f6adf509abf193e8b3c8acae19
Marek Dano <mk.dano@gmail.com>
2019-04-17 19:58:38 +0100

    Add LoginPage and NotFoundPage to router in ofcourse-app.

--- ofcourse-app/src/App.js
+++ ofcourse-app/src/App.js
@@ -3,16 +3,18 @@ import { Router, Redirect } from '@reach/router';
 import CourseListPage from './pages/CourseListPage';
 import CourseDetailPage from './pages/CourseDetailPage';
 import LoginPage from './pages/LoginPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 const App = () => {
-	return <LoginPage />;
-	// return (
-	// 	<Router>
-	// 		<Redirect noThrow from ="/" to="/courses" />
-	// 		<CourseListPage path="/courses" />
-	// 		<CourseDetailPage path="/courses/:courseId" />
-	// 	</Router>
-	// );
+	return (
+		<Router>
+			<LoginPage path="/login" />
+			<Redirect noThrow from ="/" to="/courses" />
+			<CourseListPage path="/courses" />
+			<CourseDetailPage path="/courses/:courseId" />
+			<NotFoundPage default />
+		</Router>
+	);
 }
 
 export default App;
