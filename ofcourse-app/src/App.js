ccae350be55fab7b686b21d8771ce620955fbe41
Marek Dano <mk.dano@gmail.com>
2019-05-02 21:46:18 +0100

    Create LessonPage and add it to the router in the ofcourse-app.

--- ofcourse-app/src/App.js
+++ ofcourse-app/src/App.js
@@ -4,6 +4,7 @@ import CourseListPage from './pages/CourseListPage';
 import CourseDetailPage from './pages/CourseDetailPage';
 import LoginPage from './pages/LoginPage';
 import NotFoundPage from './pages/NotFoundPage';
+import LessonPage from './pages/LessonPage';
 
 const App = () => {
 	return (
@@ -11,7 +12,9 @@ const App = () => {
 			<LoginPage path="/login" />
 			<Redirect noThrow from ="/" to="/courses" />
 			<CourseListPage path="/courses" />
-			<CourseDetailPage path="/courses/:courseId" />
+			<CourseDetailPage path="/courses/:courseId">
+				<LessonPage path="/lessons/:lessonId"/>
+			</CourseDetailPage>
 			<NotFoundPage default />
 		</Router>
 	);
---
+++ ofcourse-app/src/pages/LessonPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LessonPage = ({ lessonId }) => {
+	return <div>{lessonId}</div>
+};
+
+export default LessonPage;
